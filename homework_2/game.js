08e4486b91ebf7c4eaccfb6f16db02ecd4224846
anton2009danilov <anton2009danilov@yandex.ru>
2020-11-05 05:43:07 +0300

    исправления в тексте игры

--- homework_2/game.js
+++ homework_2/game.js
@@ -1,36 +1,27 @@
-const { randomInt } = require('crypto');
+// const { randomInt } = require('crypto');
 const readline = require('readline');
 const _ = require('lodash');
-
-
-// rl.on('line', function (cmd) {
-//     console.log('You just typed: ' + cmd);
-//     if (cmd === 'quit') {
-//         rl.close();
-//     }
-// });
-
-
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-const question = 'Орёл или решка?\n1 - орёл\n2 - решка\n3 - закончить игру\n';
+// const question = 'Орёл или решка?\n1 - орёл\n2 - решка\n3 - закончить игру\n';
+const question = 'Орёл или решка?\n1 - орёл\n2 - решка\n';
 
 rl.question(question, function(answer) {
     let result = _.sample(['1', '2']);
-    console.log (result);
-    console.log (answer === '1');
+    // console.log (result);
+    // console.log (answer === '1');
     
     let result_word; 
     let answer_word;
     
     if (result === '1')
-        result_word = 'орёл';
+        result_word = 'орёл\n';
     else
-        result_word = 'решка';
+        result_word = 'решка\n';
 
     if (answer === '1')
         answer_word = 'орёл';
@@ -38,9 +29,9 @@ rl.question(question, function(answer) {
         answer_word = 'решка';
 
     if (answer === result)
-        end_game_words = 'победили, удача на Вашей стороне!';
+        end_game_words = 'победили, удача на Вашей стороне!\n';
     else
-        end_game_words = 'проиграли, повезёт в другой раз';
+        end_game_words = 'проиграли, повезёт в другой раз\n';
 
     
 
