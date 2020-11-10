0c0d94baf4831414ec93a93a8908a6356129f572
anton2009danilov <anton2009danilov@yandex.ru>
2020-11-10 15:01:05 +0300

    Перевод сохраняется одной строкой

--- homework_3/ex2/exercise_2.js
+++ homework_3/ex2/exercise_2.js
@@ -3,13 +3,12 @@ const express = require('express');
 const app = express();
 const https = require('https');
 const fs = require('fs');
+// const fs_async = require('fs').promises;
 
 const IAM_TOKEN = 't1.9f7L7euelZrPzpvNyc2ckpOZksyTnZfJxuX090orVwL67094W9bd9PcKWlQC-u9PeFvW.CcvEsi3ancoVgCHHAW_BO7LPS-6tZDMUXFQrkMvAWVWLKBlS11qpsKllciKro0Yx3zWkijYWmE7X30ui1z1lBw';
 const FOLDER = 'b1gr1rrj3mgiq20uild5'
-// let eng_string = 'Mother was washing frame';
 
-async function translate(str) {
-    let result;
+const translate = function(str) {
     const options = {
         hostname: 'translate.api.cloud.yandex.net',
         path: '/translate/v2/translate/',
@@ -20,26 +19,14 @@ async function translate(str) {
         }
     }
     
-    // let promise = new Promise((resolve, reject) => {
-    //     const req = https.request(options, (res) => {
-    //         res.setEncoding('utf8');
-    //         res.on('data', function (chunk) {
-    //             fs.writeFile('translate_result.json', chunk, ["utf8"], (err) => {
-    //                 if (err) throw err;
-    //                 console.log('The file has been saved!');
-    //             });
-    //         });
-    //     })
-    //     return true;
-    // });
-
     const req = https.request(options, (res) => {
-        res.setEncoding('utf8');
         res.on('data', function (chunk) {
-            fs.writeFile('translate_result.json', chunk, ["utf8"], (err) => {
+            result = JSON.parse(chunk);
+            fs.writeFile('translate_result.txt', result.translations[0].text, ["utf8"], (err) => {
                 if (err) throw err;
                 console.log('The file has been saved!');
-            });
+              });
+
         });
     })
     
@@ -49,8 +36,6 @@ async function translate(str) {
         "targetLanguageCode": "ru"
     }`);
     
-    // console.log(req);
-
     req.end();
 }
 
@@ -59,11 +44,9 @@ const translateRouter = express.Router();
 translateRouter.get('/:str', (req, res) => {
     const str = req.params.str;
     translate(str);
-    // console.log(str, result);
 
-    let result = fs.readFileSync('translate_result.json', 'utf8');
-    // console.log(str, result);
-    result = JSON.parse(result);
+    let result = fs.readFileSync('translate_result.txt', 'utf8');
+
     res.send(result);
 });
 
--- homework_3/ex2/translate.js
+++ homework_3/ex2/translate.js
@@ -3,7 +3,9 @@ const fs = require('fs');
 
 const IAM_TOKEN = 't1.9f7L7euelZrPzpvNyc2ckpOZksyTnZfJxuX090orVwL67094W9bd9PcKWlQC-u9PeFvW.CcvEsi3ancoVgCHHAW_BO7LPS-6tZDMUXFQrkMvAWVWLKBlS11qpsKllciKro0Yx3zWkijYWmE7X30ui1z1lBw';
 const FOLDER = 'b1gr1rrj3mgiq20uild5'
-let eng_string = 'Mother was washing frame';
+let eng_string = 'It eat it';
+// let eng_string = 'Mother was washing frame';
+
 
 const translate = function(str) {
     const options = {
@@ -16,14 +18,12 @@ const translate = function(str) {
         }
     }
     
+    let result;
+
     const req = https.request(options, (res) => {
-        // console.log('STATUS: ' + res.statusCode);
-        // console.log('HEADERS: ' + JSON.stringify(res.headers));
-        // res.setEncoding('utf8');
-        // console.log(options);
         res.on('data', function (chunk) {
-            // console.log('BODY: ' + chunk);
-            fs.writeFile('translate_result.json', chunk, ["utf8"], (err) => {
+            result = JSON.parse(chunk);
+            fs.writeFile('translate_result.txt', result.translations[0].text, ["utf8"], (err) => {
                 if (err) throw err;
                 console.log('The file has been saved!');
               });
@@ -39,6 +39,7 @@ const translate = function(str) {
     req.end();
 }
 
-// translate(eng_string);
+translate(eng_string);
 
-console.log(fs.readFileSync('translate_result.json', 'utf8'));
+setTimeout(() => console.log(fs.readFileSync('translate_result.txt', 'utf8')), 1000);
+// console.log(fs.readFileSync('translate_result.txt', 'utf8'));
